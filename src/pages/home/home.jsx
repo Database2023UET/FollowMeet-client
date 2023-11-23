95a9140afb369a27ea9004de550e8002fb83c97e
Vinh Hoang Cong <22026501@vnu.edu.vn>
2023-11-23 19:32:16 +0700

    add post input

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,11 @@
 import Posts from "../../components/posts/posts";
+import InputPost from "../../components/share/inputPost";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
+      <InputPost />
       <Posts posts={null} />
     </div>
   );
