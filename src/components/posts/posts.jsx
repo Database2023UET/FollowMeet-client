b64dd9152ff705376dde874946fb7aa0d0227ada
Hoàng Công Vinh <haxorus5224@gmail.com>
2023-12-03 13:42:16 +0700

    connect to posts on api

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -6,48 +6,11 @@ import { AuthContext } from "../../context/authContext";
 
 const Posts = ({ posts }) => {
   //thay posts = [] bằng props posts
-  const tmpPosts = [
-    {
-      postId: 1,
-      ownerId: 1,
-      createdAt: "2021-09-01T00:00:00.000Z",
-      contentText: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      contentImg:
-        "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-    {
-      postId: 4,
-      ownerId: 2,
-      createdAt: "2021-09-01T00:00:00.000Z",
-      contentText:
-        "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
-      contentImg:
-        "https://fastly.picsum.photos/id/703/1000/1000.jpg?hmac=cRmL65OyDF7lg_MSgYgUc2ZNYJ3avrVNgRHVmT3TG4M",
-    },
-  ];
-
-  const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
-
-  const { currentUser } = useContext(AuthContext);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const res = await axios.get(
-          `${API_ENDPOINT}/api/post/getPosts?userId=${currentUser.id}`
-        );
-        // console.log(res);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchPosts();
-  }, []);
 
   return (
     <div className="posts">
-      {tmpPosts.map((post) => (
-        <Post post={post} key={post.postId} />
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
       ))}
     </div>
   );
