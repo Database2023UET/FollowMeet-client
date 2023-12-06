23cdb319ea6bd68f5274a94a628cc80de2296828
Hoàng Công Vinh <haxorus5224@gmail.com>
2023-12-06 18:25:33 +0700

    visually update the updateinfo

--- src/main.jsx
+++ src/main.jsx
@@ -12,13 +12,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <DarkModeContextProvider>
       <AlertContextProvider>
-        <UpdateInfoContextProvider>
         <AuthContextProvider>
-          <CloudinaryContextProvider>
-            <App />
-          </CloudinaryContextProvider>
+          <UpdateInfoContextProvider>
+            <CloudinaryContextProvider>
+              <App />
+            </CloudinaryContextProvider>
+          </UpdateInfoContextProvider>
         </AuthContextProvider>
-        </UpdateInfoContextProvider>
       </AlertContextProvider>
     </DarkModeContextProvider>
   </React.StrictMode>
